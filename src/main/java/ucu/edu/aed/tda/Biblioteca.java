8890626f828b3bf5ffe2b52d4de4d975d0e9c299
Guillermo Gonzalez <guillermo.gonzalezt@correo.ucu.edu.uy>
2026-03-25 09:00:21 -0300

    ej17 parte 2 sub-equipo B

--- src/main/java/ucu/edu/aed/tda/Biblioteca.java
+++ src/main/java/ucu/edu/aed/tda/Biblioteca.java
@@ -1,5 +1,49 @@
 package ucu.edu.aed.tda;
 
-public class Biblioteca extends Lista {
+public class Biblioteca extends TDAListaEnlazada {
+
+    TDAListaEnlazada<Libro> listalibros = new TDAListaEnlazada<>();
+    TDAListaEnlazada<Libro> listalibrosenprestamo = new TDAListaEnlazada<>();
+
+    public void RegistrarPrestamo(int ISBN) {
+        
+        Nodo<Libro> cabeza = listalibros.cabeza;
+        Nodo<Libro> actual = cabeza;
+
+        while (actual != null) {
+            if (actual.getDato().equals(ISBN)) {
+                if (actual.getDato().stock >0) {
+                    actual.getDato().stock--;
+                    System.out.println("Préstamo registrado para ISBN: " + actual.getDato().ISBN);
+                    System.out.println("Stock restante: " + actual.getDato().stock);
+                    listalibrosenprestamo.agregar(actual.getDato());
+                    return;
+                }
+                else {
+                    System.out.println("No hay stock disponible para ISBN: " + actual.getDato().ISBN);
+                    return;
+                }
+            }
+            actual = actual.getSiguiente();
+        }
+        System.out.println("ISBN no encontrado: " + ISBN);
+    }
+
+    public void RegistrarDevolucion(int ISBN) {
+        Nodo<Libro> cabeza = listalibros.cabeza;
+        Nodo<Libro> actual = cabeza;
+
+        while (actual != null) {
+            if (actual.getDato().equals(ISBN)) {
+                actual.getDato().stock++;
+                System.out.println("Devolución registrada para ISBN: " + actual.getDato().ISBN);
+                System.out.println("Stock actualizado: " + actual.getDato().stock);
+                listalibrosenprestamo.eliminar(actual.getDato());
+                return;
+            }
+            actual = actual.getSiguiente();
+        }
+        System.out.println("ISBN no encontrado: " + ISBN);
+    }
 
 }
