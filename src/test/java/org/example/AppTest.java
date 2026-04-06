265dc2a798cf8c5ee5e8dbfd54c338012027b8f5
fedehmartinez <federicoh.martinez@correo.ucu.edu.uy>
2026-04-06 10:46:27 -0300

    Agrego comentario en AppTest

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,10 +1,8 @@
 package org.example;
 
 import junit.framework.Test;
-import junit.framework.TestCase;
+import junit.framework.TestCase;  // Usar este importa para los tests
 import junit.framework.TestSuite;
-
-import java.util.Objects;
 
 /**
  * Unit test for simple App.
