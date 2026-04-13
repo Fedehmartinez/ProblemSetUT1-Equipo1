b0936dc9a0a7c66c5ac1e195d8555c823ede3de2
Hernan Lopez <hlhernano@gmail.com>
2026-04-13 08:05:26 -0300

    ejercicio26

--- src/main/java/ucu/edu/aed/ejercicio26/ejercicio26.java
+++ src/main/java/ucu/edu/aed/ejercicio26/ejercicio26.java
@@ -3,24 +3,25 @@ package ucu.edu.aed.ejercicio26;
 import java.util.List;
 
 import ucu.edu.aed.tda.implementaciones.PilaListaEnlazada;
+import ucu.edu.aed.tda.implementaciones.TDAListaEnlazada;
 
 public class ejercicio26 {
-
-    public boolean controlCorchetes(List<Character> listaDeEntrada) {
-        PilaListaEnlazada<Character> pila = new PilaListaEnlazada<>();
-        for (Character c : listaDeEntrada) {
-            if (c == '{') {
-                pila.push(c);
-            } else if (c == '}') {
-                if (pila.esVacia()) {
-                    return false;
-                }
-                if (c == '}'){
-                    pila.pop();
-                }
+    public boolean controlCorchetes(TDAListaEnlazada<Character> listaDeEntrada) {
+    PilaListaEnlazada<Character> pila = new PilaListaEnlazada<>();
+    for (int i = 0; i < listaDeEntrada.tamaño(); i++) {
+        Character c = listaDeEntrada.obtener(i);
+        if (c == '{') {
+            pila.push(c);
+        } else if (c == '}') {
+            if (pila.esVacia()) {
+                return false;
+            }
+            if (c == '}'){
+                pila.pop();
             }
         }
-        return pila.esVacia();
+    }
+    return pila.esVacia();
     }
 }
 
